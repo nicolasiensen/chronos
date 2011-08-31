4f892bc84e673525f81497a17eda7c9bd045bcae
Nícolas Iensen <nicolas@quavio.com.br>
2011-08-30 22:23:36 -0300

    Including cucumber. Change rails version to the 3.0.10

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Chronos::Application.config.secret_token = '95874cbced120a029611f14f577fc49df877ebc305fcb6085e3ef3e978e90bbe7f1c2d2c7e066dc9bddf2df5563f45c9c6a9b0647f669a37a0da20da0c1abbfe'
+Chronos::Application.config.secret_token = '67ea20d24373ff7ae5a1cce0d1d80ce85195b1f2da38a91be464c53f7fbf25e5b0e1679923d713a11eeb0eefe8e51cbfd7548f5449a6123fa8a738a0c65dcd59'
