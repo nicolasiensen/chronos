b46e57546c7dbaa23475e6638ea64e3dc696a95c
Nícolas Iensen <nicolas@quavio.com.br>
2011-08-31 11:16:24 -0300

    Adding fixtures

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ActiveRecord::Base
   def update_from_cucumber
     json = JSON.parse Net::HTTP.get(URI.parse(source_url))
     json.fetch("features").each do |feature|
-      UserStory.create_from_sentence feature.fetch("description")
+      user_stories.create_from_sentence feature.fetch("description")
     end
   end
 
