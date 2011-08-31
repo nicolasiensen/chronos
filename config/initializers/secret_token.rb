85e954962a577f2d5af5dfb71b0377806c1cfb7a
Nícolas Iensen <nicolas@quavio.com.br>
2011-08-30 21:51:52 -0300

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chronos::Application.config.secret_token = '95874cbced120a029611f14f577fc49df877ebc305fcb6085e3ef3e978e90bbe7f1c2d2c7e066dc9bddf2df5563f45c9c6a9b0647f669a37a0da20da0c1abbfe'
